0bd75f9d50346cfa9321d23ff9af2fd17008cb12
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2024-03-09 14:08:17 +0530

    changes

--- components/setUserInfo/userForm.tsx
+++ components/setUserInfo/userForm.tsx
@@ -38,6 +38,7 @@ export function UserProfileForm({ fetchUser }: any) {
   const [showLocationSearch, setShowLocationSearch] = useState(false)
   const { supabaseUser } = useAuth({})
   const [showWelcomeMessage, setShowWelcomeMessage] = useState(false)
+  const [uploadingProfile, setUploadingProfile] = useState(false)
 
   const onSubmit = async (data: any) => {
     await axios.post("/api/supabase/update", {
@@ -140,7 +141,7 @@ export function UserProfileForm({ fetchUser }: any) {
   if (showWelcomeMessage) {
     return (
       <div className="mt-2">
-        <hr className="my-1"/>
+        <hr className="my-1" />
         <p className="font-bold my-2">
           On the next page you can opt in to provide more information about
           yourself in the form of “stamps” which will increase your score. This
@@ -263,19 +264,33 @@ export function UserProfileForm({ fetchUser }: any) {
           accept="image/*"
           {...register("profilePhoto", { required: true })}
           onChange={async (e: any) => {
+            setUploadingProfile(true)
             const file = e.target.files[0]
-            const fileToBlob = async (files) => new Blob([new Uint8Array(await files.arrayBuffer())], {type: files.type });
+            const fileToBlob = async (files) =>
+              new Blob([new Uint8Array(await files.arrayBuffer())], {
+                type: files.type,
+              })
 
             const { data, error } = await supabase.storage
               .from("supabase-pfp")
-              .upload(`images/${supabaseUser?.email}.png`, await fileToBlob(file), {
-                cacheControl: "3600",
-                upsert: true,
-              })
+              .upload(
+                `images/${supabaseUser?.email}.png`,
+                await fileToBlob(file),
+                {
+                  cacheControl: "3600",
+                  upsert: true,
+                }
+              )
             setProfilePic(data?.path ?? "")
+            setUploadingProfile(false)
           }}
           className="mt-1 block w-full rounded-md border border-gray-600 px-3 py-2 shadow-sm dark:bg-black dark:text-white sm:text-sm"
         />
+        {uploadingProfile && (
+          <>
+            <p className="animate-pulse">Uploading your profile pic</p>
+          </>
+        )}
         {!Boolean(profilePic) && errors.profilePhoto && (
           <p className="mt-2 text-sm text-red-600">Profile photo is required</p>
         )}
@@ -372,7 +387,7 @@ export function UserProfileForm({ fetchUser }: any) {
 
       <button
         type="submit"
-        disabled={!isUsernameAvailable}
+        disabled={!isUsernameAvailable || uploadingProfile}
         className={`w-full rounded-md bg-blue-500 px-4 py-2 text-white ${
           isUsernameAvailable === true ? `opacity-1` : `opacity-70`
         }`}
