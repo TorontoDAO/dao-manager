227eb356103878c8cf2cbc66838a8652a550c1d9
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2024-03-14 03:35:15 +0530

    changes

--- components/member/index.tsx
+++ components/member/index.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react"
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api"
 import axios from "axios"
 import dayjs from "dayjs"
 
 import { supabase } from "@/lib/supabase"
-import {ShowUserInfo} from './showUserInfo'
+
+import { ShowUserInfo } from "./showUserInfo"
 
 const DaoCard = ({ item }: any) => {
   const [profilepic, setProfilePic] = useState("")
@@ -21,7 +23,6 @@ const DaoCard = ({ item }: any) => {
   }
   return (
     <div className="rounded-lg border border-gray-200 p-4 dark:border-gray-800">
-
       <img
         src={`${profilepic}?string=${Math.random()}`}
         alt="profile"
@@ -38,6 +39,31 @@ const DaoCard = ({ item }: any) => {
 export const Members = () => {
   const [accounts, setAccounts] = useState([])
 
+  function extractLatLng(inputStr = "") {
+    if ((inputStr as any)?.latitude) {
+      return {
+        lat: (inputStr as any)?.latitude,
+        lng: (inputStr as any)?.longitude,
+      }
+    }
+    // Use a regular expression to find matches for latitude and longitude in the input string
+    const latLngRegex =
+      /Latitude:\s*([-+]?[0-9]*\.?[0-9]+),\s*Longitude:\s*([-+]?[0-9]*\.?[0-9]+)/
+    const match = inputStr.match(latLngRegex)
+
+    if (match) {
+      // Convert the matched latitude and longitude strings to numbers
+      const latitude = parseFloat(match[1])
+      const longitude = parseFloat(match[2])
+
+      // Return an object with the latitude and longitude
+      return { lat: latitude, lng: longitude }
+    } else {
+      // If the input string doesn't match the expected format, return null or an error
+      return null
+    }
+  }
+
   useEffect(() => {
     ;(async () => {
       const {
@@ -45,6 +71,21 @@ export const Members = () => {
       } = await axios.post("/api/supabase/fetchDaoUser")
       setAccounts(data)
     })()
+  }, [])
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCH9P8gyasf9bIWxk6bXUnk833jqyQudwI",
+  })
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map: any) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null)
   }, [])
 
   const [userData, setUserData] = useState(null)
@@ -68,6 +109,38 @@ export const Members = () => {
             recognized and rewarded for their generosity.
           </p>
         </div>
+        {isLoaded && accounts?.length !== 0 && (
+          <GoogleMap
+            mapContainerStyle={{
+              width: "100%",
+              height: "400px",
+              borderRadius: 10,
+              marginTop: 10,
+              marginBottom: 10,
+            }}
+            center={{ lat: 0, lng: 0 }}
+            zoom={2}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            {accounts.map((item: any) => {
+              const isLocation =
+                item?.dao_info?.location?.latitude ||
+                (typeof item?.dao_info?.location === "string" &&
+                  item?.dao_info?.location.length !== 0)
+              return (
+                <>
+                  {isLocation && (
+                    <MarkerF
+                      title={item?.username}
+                      position={extractLatLng(item?.dao_info?.location) as any}
+                    />
+                  )}
+                </>
+              )
+            })}
+          </GoogleMap>
+        )}
         <div className="grid grid-cols-2 gap-3 md:grid-cols-4">
           {accounts.map((item: any) => (
             <div
@@ -80,9 +153,13 @@ export const Members = () => {
             </div>
           ))}
         </div>
-        <ShowUserInfo data={userData} open={Boolean(userData)} onClose={()=>{
-          setUserData(null)
-        }} />
+        <ShowUserInfo
+          data={userData}
+          open={Boolean(userData)}
+          onClose={() => {
+            setUserData(null)
+          }}
+        />
       </div>
     </>
   )
--- components/member/showUserInfo.tsx
+++ components/member/showUserInfo.tsx
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import React, { useCallback, useState } from "react"
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api"
 import axios from "axios"
 import _ from "lodash"
 
@@ -11,7 +12,50 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet"
 
+function extractLatLng(inputStr = "") {
+  if (inputStr?.latitude) {
+    return {
+      lat: inputStr?.latitude,
+      lng: inputStr?.longitude,
+    }
+  }
+  // Use a regular expression to find matches for latitude and longitude in the input string
+  const latLngRegex =
+    /Latitude:\s*([-+]?[0-9]*\.?[0-9]+),\s*Longitude:\s*([-+]?[0-9]*\.?[0-9]+)/
+  const match = inputStr.match(latLngRegex)
+
+  if (match) {
+    // Convert the matched latitude and longitude strings to numbers
+    const latitude = parseFloat(match[1])
+    const longitude = parseFloat(match[2])
+
+    // Return an object with the latitude and longitude
+    return { lat: latitude, lng: longitude }
+  } else {
+    // If the input string doesn't match the expected format, return null or an error
+    return null
+  }
+}
+
 export const ShowUserInfo = ({ data, open, onClose }: any) => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCH9P8gyasf9bIWxk6bXUnk833jqyQudwI",
+  })
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+  console.log(data?.location, data, extractLatLng(data?.location))
+  const isLocation =
+    data?.location?.latitude ||
+    (typeof data?.location === "string" && data?.location.length !== 0)
   return (
     <>
       <Sheet open={open} onOpenChange={onClose}>
@@ -27,8 +71,27 @@ export const ShowUserInfo = ({ data, open, onClose }: any) => {
               About Me
             </label>
             <div className="flex items-center">
-              <p>{data?.introduce??`About me is Empty for ${data?.username} :)`}</p>
+              <p>
+                {data?.introduce ??
+                  `About me is Empty for ${data?.username} :)`}
+              </p>
             </div>
+            {isLoaded && isLocation && (
+              <GoogleMap
+                mapContainerStyle={{
+                  width: "100%",
+                  height: "300px",
+                  borderRadius: 10,
+                  marginTop: 10,
+                }}
+                center={extractLatLng(data?.location)}
+                zoom={10}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+              >
+                <MarkerF position={extractLatLng(data?.location)} />
+              </GoogleMap>
+            )}
           </div>
         </SheetContent>
       </Sheet>
