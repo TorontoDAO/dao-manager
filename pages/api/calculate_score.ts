999cd20740548e7894711277c6adc0abcb5c0ac4
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-06-05 01:32:09 +0530

    changes

--- pages/api/calculate_score.ts
+++ pages/api/calculate_score.ts
@@ -1,5 +1,6 @@
 import NextCors from "nextjs-cors"
 
+import { calculateScores } from "./utils/fetchScores"
 import { supabase } from "./utils/supabase"
 
 const fetch_score = async (req: any, res: any) => {
@@ -10,51 +11,15 @@ const fetch_score = async (req: any, res: any) => {
     optionsSuccessStatus: 200, // Some legacy browsers choke on 204
   })
   const { user_id } = req.body
-  const { data: dapp_users } = await supabase
+  const { error, data } = await supabase
     .from("dapp_users")
-    .select("*,users:user_id(*),dapps:dapp_id(*)")
-    .match({
-      user_id,
-    })
-  const { error, data: stampData } = await supabase
-    .from("dapp_stamptypes")
-    .select("*,stamptypes:stamptype_id(*)")
-    .match({
-      dapp_id: dapp_users?.[0]?.dapp_id,
-    })
-  const { data: scoreData } = await supabase
-    .from("stampscore_dapps")
-    .select("*,stampscore_schemas:schema_id(*)")
-    .match({
-      dapp_id: dapp_users?.[0]?.dapp_id,
-    })
-  const { data: stampScores } = await supabase
-    .from("stampscores_available")
     .select("*")
-    .match({
-      schema_id: scoreData?.[0]?.schema_id,
-    })
-
-  const { data: stampsList } = await supabase.from("stamps").select("*").match({
-    created_by_user_id: dapp_users?.[0]?.user_id,
+    .neq("username", null)
+  const score = await calculateScores({
+    user_id: user_id,
+    location: data?.[0].user_data?.location,
   })
-
-  const stampsToSend = stampData ?? []
-
-  const allStampIds = (stampsList ?? []).map((item: any) => item.stamptype)
-
-  const stampScore = [
-    ...stampsToSend.filter((item) =>
-      allStampIds?.includes(item?.stamptypes?.id)
-    ),
-  ].reduce((curr, item) => {
-    const scoreData = (stampScores ?? []).find(
-      (_) => _.stamptype_id === item.stamptype_id
-    )
-    const scoreToAdd = scoreData?.score ?? 0
-    return scoreToAdd + curr
-  }, 0)
-  res.send(stampScore)
+  return score
 }
 
 export default fetch_score
