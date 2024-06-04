25952e893e7561b130b157f744e80df80ade4a98
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-06-05 02:30:29 +0530

    changes

--- pages/api/calculate_score.ts
+++ pages/api/calculate_score.ts
@@ -4,22 +4,17 @@ import { calculateScores } from "./utils/fetchScores"
 import { supabase } from "./utils/supabase"
 
 const fetch_score = async (req: any, res: any) => {
-  await NextCors(req, res, {
-    // Options
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    origin: "*", // Allow all origins
-    optionsSuccessStatus: 200, // Some legacy browsers choke on 204
-  })
   const { user_id } = req.body
   const { error, data } = await supabase
     .from("dapp_users")
     .select("*")
     .neq("username", null)
+  console.log(data)
   const score = await calculateScores({
     user_id: user_id,
     location: data?.[0].user_data?.location,
   })
-  return score
+  res.send(score);
 }
 
 export default fetch_score
